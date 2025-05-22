e6cb5c7850caa42c0fa55b503906d297a438bda1
bohorquezandrea <bohorquezandrea99@gmail.com>
2025-05-22 10:46:36 -0500

    Ejemplos básicos en archivo 00-helloworld.js

---
+++ basic_ab/00-helloworld.js
@@ -0,0 +1,18 @@
+// Esto es un comentario simple 
+
+/* Esto 
+es un 
+comentario
+en varias lîneas*/ 
+
+console.log("Buenas buenass")
+
+console.log(5)
+console.log("5")
+
+console.log(5 - 2)
+console.log(5 + 2)
+console.log(5 % 2)
+console.log(5 / 2)
+console.log(5 * 2)
+console.log(5 ** 2) // 5 elevado a la 2`d
