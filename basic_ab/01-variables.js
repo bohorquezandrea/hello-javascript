dbcde7e08357b5cc290ae00a503a135e507eaeca
bohorquezandrea <bohorquezandrea99@gmail.com>
2025-05-22 11:01:40 -0500

    Agrega ejemplos de variables con var, let y const

---
+++ basic_ab/01-variables.js
@@ -0,0 +1,22 @@
+// var
+// DEPRECATED
+
+var helloWorld = "Alo, JavaScript"
+console.log(helloWorld)
+
+helloWorld = "ALOOO, JAVASCRIPT!!??"
+console.log(helloWorld)
+
+// let
+
+let helloWorld2 = "Alo, JavaScript2"
+console.log(helloWorld2)
+
+helloWorld2 = "ALOOO, JAVASCRIPT2!!??"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = "Alo, JavaScript3"
+console.log(helloWorld3)
+
