36f6f93324e7006dbd34662832ce53ae235a9db7
bohorquezandrea <bohorquezandrea99@gmail.com>
2025-05-23 09:28:42 -0500

    tipos de datos y ejercicios resueltos

---
+++ basic_ab/02-datatypes.js
@@ -0,0 +1,41 @@
+// Cadenas de texto (string)
+let name = "Andrea Bohorquez"
+let alias = 'Andreita'
+let email = `bohorquezandrea99@gmail.com`
+
+// Números (number)
+let age = 26 // Entero
+let height = 1.63 // Decimal 
+
+// Boolean
+let isStudent = true 
+let isTeacher = false  
+
+// Undefined 
+let undefinedValue
+console.log(undefinedValue) // undefined
+
+// Null
+let nullValue = null // ausencia de valor 
+
+// Symbol 
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+
+let bigInt = BigInt(182763876148761874638276587235982734)
+let bitInt2 = 172094180397409273057190348019834091n 
+
+// Datatypes (primitivos)
+console.log(typeof name) // string
+console.log(typeof alias) // string
+console.log(typeof email) // string
+console.log(typeof age) // number
+console.log(typeof height) // number
+console.log(typeof isStudent) // boolean
+console.log(typeof isTeacher) // boolean
+console.log(typeof undefinedValue) // undefined
+console.log(typeof nullValue) // object
+console.log(typeof mySymbol) // symbol
+console.log(typeof bigInt) // bigint
+console.log(typeof bitInt2) // bigint
---
+++ basic_ab/03-beginner-exercises.js
@@ -0,0 +1,57 @@
+// 1. Escribe un comentario en una línea
+
+// Hola
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+hola
+esto es un comenatario
+en varias lineas
+*/ 
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos 
+
+let name = "Andrea"
+let number = 3
+let boolean = true
+let symbol = Symbol("symbol")
+let bigInt = 918237947934279891374n
+let undefinedValue = undefined
+let nulo = null 
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(name, number, boolean, symbol, bigInt, undefinedValue, nulo)
+
+// 5. Imprime por consola el tipo de dato de cada una de las variables
+
+console.log(typeof name, typeof number, typeof boolean, typeof symbol, typeof bigInt, typeof undefinedValue, typeof nulo)
+
+// 6. Modifica los valores de las variables por otros del mismo tipo 
+
+name = "Juan"
+number = 7
+boolean = false
+symbol = Symbol("nuevo simbolo")
+bigInt = 12345678901234567890n
+undefinedValue = "esto es un string"
+nulo = 0
+
+// 7. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const name2 = "Andrea"
+const number2 = 3
+const boolean2 = true
+const symbol2 = Symbol("symbol")
+const bigInt2 = 918237947934279891374n
+const undefinedValue2 = undefined
+const nulo2 = null 
+
+// 9. Modifica los valores de las constantes
+
+/* va a arrojar error */ 
+
+// 10. Comenta las lineas de código que no sean necesarias 
+
+
